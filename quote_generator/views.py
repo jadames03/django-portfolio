c161980dcdf35eac9d511ee86b5dba335eeb7a57
Justin Adames <adamesj03@gmail.com>
2022-01-07 19:55:25 -0500

    initial

--- quote_generator/views.py
+++ quote_generator/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'quote_generator/index.html')
-def about(request):
-    return render(request, 'quote_generator/about.html')
+# def about(request):
+#     return render(request, 'quote_generator/about.html')
