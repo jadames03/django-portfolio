b15386ce18059c31c91c18ca536b5b4db095b1f8
Justin Adames <adamesj03@gmail.com>
2022-01-11 01:24:53 -0500

    initial commit

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'quote_generator'
+    'quote_generator',
+    'portfolio_site',
 ]
 
 MIDDLEWARE = [
@@ -116,8 +117,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_DIRS = [
+    BASE_DIR / 'quote_generator' / 'static',
+    BASE_DIR / 'movie_booking' / 'static',
+]
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
