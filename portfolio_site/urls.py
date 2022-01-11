f970c9fb054f6f19597d31799836e661095eb4c5
Justin Adames <adamesj03@gmail.com>
2022-01-11 01:24:53 -0500

    initial commit

--- portfolio_site/urls.py
+++ portfolio_site/urls.py
@@ -1,7 +1,9 @@
+from django.contrib import admin
 from django.urls import path
 from quote_generator import views
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', views.index, name='index'),
     # path('about/', views.about, name='about'),
 ]
