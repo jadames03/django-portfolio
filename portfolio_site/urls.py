f512af3d8cbe672cfa214e813989ae59c82986a6
Justin Adames <adamesj03@gmail.com>
2025-02-13 04:44:21 -0500

    add and remove routes

--- portfolio_site/urls.py
+++ portfolio_site/urls.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-from quote_generator import views as quoteView
-from exchange_rate import views as exchangeView
 from portfolio import views
 from django.conf.urls.static import static
 from django.conf import settings
@@ -9,10 +7,12 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('quote_generator', quoteView.index, name='index'),
-    path('exchange_rate/', exchangeView.index, name='index'),
     path('blog/', include('blog.urls')),
     path('', views.home, name="home"),
     # path('about/', views.about, name='about'),
+    path('debug-media/', views.debug_media, name='debug-media'),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Static files are handled by whitenoise
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
