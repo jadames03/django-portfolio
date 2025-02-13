9af0704d33fd746e3262a68523519df5b276baac
Justin Adames <adamesj03@gmail.com>
2025-02-13 04:42:35 -0500

    update settings.py

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@-2awfvpz$u#synq%#6(q+8xk3sbybno683m&mdda30yj*+km@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['pg8-ingenious-galilei.circumeo-apps.net', '127.0.0.1', 'localhost']
 
@@ -38,14 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'quote_generator',
     'portfolio',
     'blog',
-    'exchange_rate',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,11 +120,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_DIRS = [
-    BASE_DIR / 'quote_generator' / 'static',
-    BASE_DIR / 'exchange_rate' / 'static',
-    BASE_DIR / 'movie_booking' / 'static',
+STATICFILES_DIRS = [
+    BASE_DIR / 'portfolio' / 'static',
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
