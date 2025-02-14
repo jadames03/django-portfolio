3c2afb666937ac5f01d0273c7d6fa3d55a7afef3
Justin Adames <adamesj03@gmail.com>
2025-02-14 03:30:46 -0500

    attempt vercel deployment

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,11 @@
 # build_files.sh
+# Set Python path
+export PYTHONPATH="/vercel/path0:${PYTHONPATH}"
+
+# Install requirements directly with python3.9
+python3.9 -m ensurepip
 python3.9 -m pip install --upgrade pip
 python3.9 -m pip install -r requirements.txt
+
+# Run collectstatic
 python3.9 manage.py collectstatic --noinput 
