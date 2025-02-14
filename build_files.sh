81ad27486b9004948a9fd74968f8a1b915532667
Justin Adames <adamesj03@gmail.com>
2025-02-14 03:24:16 -0500

    update build

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,4 @@
 # build_files.sh
-python3 -m pip install -r requirements.txt
-python3 manage.py collectstatic --noinput 
+python3.9 -m pip install --upgrade pip
+python3.9 -m pip install -r requirements.txt
+python3.9 manage.py collectstatic --noinput 
