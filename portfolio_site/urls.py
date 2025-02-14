577a578df5a5787e9d525b3792a9573e1065823e
Justin Adames <adamesj03@gmail.com>
2025-02-14 16:33:43 -0500

    remove serve

--- portfolio_site/urls.py
+++ portfolio_site/urls.py
@@ -1,10 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
 from portfolio import views
-from django.conf.urls.static import static
-from django.conf import settings
-from django.views.static import serve
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,10 +8,8 @@ urlpatterns = [
     path('', views.home, name="home"),
     # path('about/', views.about, name='about'),
     path('debug-media/', views.debug_media, name='debug-media'),
-    path('media/<path:path>/', serve, {'document_root': settings.MEDIA_ROOT}),
-    # path('static/<path:path>/', serve, {'document_root': settings.STATIC_ROOT}),
 ]
 
 # These are not needed if using serve view
 # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
