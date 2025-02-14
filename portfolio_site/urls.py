43f300518b2d373f5a919e0551562e167db553cd
Justin Adames <adamesj03@gmail.com>
2025-02-14 01:16:32 -0500

    use serve for media and static roots

--- portfolio_site/urls.py
+++ portfolio_site/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from portfolio import views
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.static import serve
 
 
 urlpatterns = [
@@ -11,8 +12,10 @@ urlpatterns = [
     path('', views.home, name="home"),
     # path('about/', views.about, name='about'),
     path('debug-media/', views.debug_media, name='debug-media'),
+    path('media/<path:path>/', serve, {'document_root': settings.MEDIA_ROOT}),
+    path('static/<path:path>/', serve, {'document_root': settings.STATIC_ROOT}),
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-# Static files are handled by whitenoise
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# These are not needed if using serve view
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
