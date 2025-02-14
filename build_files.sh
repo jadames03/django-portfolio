924c3d1000e7d1a3154d643fa8d4775c28cc8b2c
Justin Adames <adamesj03@gmail.com>
2025-02-14 16:32:24 -0500

    update build

--- build_files.sh
+++ build_files.sh
@@ -6,7 +6,3 @@ export PYTHONPATH="/vercel/path0:${PYTHONPATH}"
 python3.9 -m ensurepip
 python3.9 -m pip install --upgrade pip
 python3.9 -m pip install -r requirements.txt
-
-# Create staticfiles directory and collect static files
-rm -rf staticfiles/*  # Clean the directory first
-python3.9 manage.py collectstatic --no-input --clear 
